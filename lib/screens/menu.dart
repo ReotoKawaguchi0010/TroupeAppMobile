bd5d7671aad94c946941c23ff2fa21895ff41b62
ReotoKawaguchi0010 <pnxde6mxu@i.softbank.jp>
2021-04-29 11:54:50 +0900

    menu

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,9 +5,21 @@ class MenuState extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       body: Container(
-        child: Center(
-          child: Text('text'),
-        ),
+        child: ListView(
+          children: [
+            Center(
+              child: ElevatedButton(
+                child: Text('button'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color(0xFFC14949)),
+                ),
+              ),
+            )
+          ],
+        ), 
       ),
     );
   }
